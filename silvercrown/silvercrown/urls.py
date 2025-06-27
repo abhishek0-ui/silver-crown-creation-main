4e87fa5e366c5301a1e479d0bee08baf47f626e2
silver-ui1 <abhishek.patkar25@gmail.com>
2025-06-27 10:23:55 +0530

    Update urls.py

--- silvercrown/silvercrown/urls.py
+++ silvercrown/silvercrown/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to Silver Crown Creation!")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # this enables the admin site
+    path('', home),  # Handles /
+    path('admin/', admin.site.urls),
     path('api/', include('shop.urls')),
 ]
 
