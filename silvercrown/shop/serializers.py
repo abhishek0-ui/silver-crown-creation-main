c60b65c51fdf6e6eeaa9f73cc863281351e4515d
silver-ui1 <abhishek.patkar25@gmail.com>
2025-06-27 15:43:10 +0530

    s

--- silvercrown/shop/serializers.py
+++ silvercrown/shop/serializers.py
@@ -2,8 +2,12 @@ from rest_framework import serializers
 from .models import Product, ProductImage
 
 class ProductImageSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(use_url=True)
-    thumbnail = serializers.ImageField(use_url=True)
+    image = serializers.SerializerMethodField()
+
+    def get_image(self, obj):
+        if obj.image:
+            return obj.image.url  # ✅ returns full Cloudinary URL
+        return None
 
     class Meta:
         model = ProductImage
@@ -11,7 +15,12 @@ class ProductImageSerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     images = ProductImageSerializer(many=True, read_only=True)
-    thumbnail = serializers.ImageField(use_url=True)  # ✅ ensures full URL
+    thumbnail = serializers.SerializerMethodField()
+
+    def get_thumbnail(self, obj):
+        if obj.thumbnail:
+            return obj.thumbnail.url  # ✅ returns full Cloudinary URL
+        return None
 
     class Meta:
         model = Product
