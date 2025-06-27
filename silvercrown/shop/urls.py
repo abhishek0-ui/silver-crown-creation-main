9845278e089ef3fddf268a6f3b80b6e9a07dafd9
silver-ui1 <abhishek.patkar25@gmail.com>
2025-06-27 11:58:39 +0530

    e

--- silvercrown/shop/urls.py
+++ silvercrown/shop/urls.py
@@ -1,13 +1,7 @@
 from django.urls import path
 from .views import product_list, product_page
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-    path('products/', product_list),     # JSON API at /products/
-    path('all/', product_page),          # HTML product list at /all/
+    path('products/', product_list),     # JSON API
+    path('', product_page),              # Rendered HTML homepage
 ]
-
-# Serve media files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- silvercrown/shop/views.py
+++ silvercrown/shop/views.py
@@ -2,13 +2,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
-from django.shortcuts import render
+from django.shortcuts import render  # also needed
 
 @api_view(['GET'])
-# def product_list(request):
-#     products = Product.objects.all()
-#     serializer = ProductSerializer(products, many=True)
-#     return Response(serializer.data)
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 def product_page(request):
     products = Product.objects.all()
