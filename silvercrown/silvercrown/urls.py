e1379c26bda43c1145760b7de1f6b800fbc89381
silver-ui1 <abhishek.patkar25@gmail.com>
2025-06-27 13:18:53 +0530

    Update urls.py

--- silvercrown/silvercrown/urls.py
+++ silvercrown/silvercrown/urls.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Welcome to Silver Crown Creation</h1><p>Visit <a href='/api/products/'>Products</a></p>")
 
 urlpatterns = [
-    path('', include('shop.urls')),         # ✅ Homepage from shop
+    path('', home),
     path('admin/', admin.site.urls),
+    path('api/', include('shop.urls')),  # 👈 this should point to shop.urls
 ]
 
 if settings.DEBUG:
