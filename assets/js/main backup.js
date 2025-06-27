b2f0bb39f6b20e47d99ce3cb04becd974be8ed06
silver-ui1 <abhishek.patkar25@gmail.com>
2025-06-27 16:51:12 +0530

    Update main backup.js

--- assets/js/main backup.js
+++ assets/js/main backup.js
@@ -174,13 +174,15 @@ async function loadProduct(index) {
   document.getElementById("stock").textContent = product.stock;
   document.getElementById("main-images").innerHTML = `<img src="${product.thumbnail}" id="main-image" />`;
 
- const thumbs = document.getElementById("thumbs");
+const thumbs = document.getElementById("thumbs");
 thumbs.innerHTML = '';
 product.images?.forEach(img => {
-  const thumb = document.createElement("div");
-  thumb.className = "pro-nav-thumb";
-  thumb.innerHTML = `<img src="${img.image}" data-src="${img.image}" />`;
-  thumbs.appendChild(thumb);
+  if (img.image !== product.thumbnail) {
+    const thumb = document.createElement("div");
+    thumb.className = "pro-nav-thumb";
+    thumb.innerHTML = `<img src="${img.image}" data-src="${img.image}" />`;
+    thumbs.appendChild(thumb);
+  }
 });
 
   const stars = Math.round(product.rating);
